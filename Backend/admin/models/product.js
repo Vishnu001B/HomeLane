9fe480d5e4c3c17d39692cb486e55477379d0a81
Ramnarayan Mandal <154451608+RamnarayanMandal@users.noreply.github.com>
2024-09-05 16:28:06 +0530

    modify the product schema

--- Backend/admin/models/product.js
+++ Backend/admin/models/product.js
@@ -12,7 +12,6 @@ const productSchema = new mongoose.Schema(
       enum: ["Premium", "Classic", "Economic"], // Corrected typo from "Ecommic" to "Economic"
       default: ["Economic"], // Default value should also be an array
     },
-    
     price: { type: Number, required: true }, // Updated to Number
     discount: { type: Number, default: 0 },
     totalPrice: { type: Number, default: 0 }, // Ensure totalPrice is a Number
@@ -37,5 +36,18 @@ const productSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// Pre-save hook to convert categories and subcategory to lowercase
+productSchema.pre("save", function (next) {
+  if (this.categories) {
+    this.categories = this.categories.toLowerCase();
+  }
+  
+  if (this.subcategory && this.subcategory.length > 0) {
+    this.subcategory = this.subcategory.map((item) => item.toLowerCase());
+  }
+  
+  next();
+});
 
 module.exports = mongoose.model("Product", productSchema);
